dc919505e7b894f752cb12fd8a6ede1c83ab6a2a
mstra <mwstrand@uci.edu>
2021-08-01 18:41:53 -0700

    u

---
+++ setup.py
@@ -0,0 +1,11 @@
+import numpy as np
+from setuptools import setup
+
+setup(
+    name='NumericalAnalysis',
+    version='0.1dev',
+    packages=['numan'],
+    author='mstrand',
+    author_email='mwstrand@uci.edu',
+    install_requires=['numpy']
+)
