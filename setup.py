56d5e574d09ff652df69947a7eeab26daa13a280
mstra <mwstrand@uci.edu>
2021-08-02 18:09:50 -0700

    u

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='mstrand',
     author_email='mwstrand@uci.edu',
     long_description ='Numerical Analysis Algorithms from class.',
-    long_description_content_type='text/x-rst',
+    long_description_content_type='text/markdown',
     install_requires=['numpy'],
     url='https://github.com/mwstrand/numerical-analysis'
 )
