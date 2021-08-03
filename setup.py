fb53cc1cfb06cd889509f3e7f34260f44454c3c0
mstra <mwstrand@uci.edu>
2021-08-02 19:45:42 -0700

    u

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
-    name='NumericalAnalysis',
+    name='numan',
     version='0.1',
-    packages=['numan'],
+    packages=find_packages(where='src'),
     author='mstrand',
     author_email='mwstrand@uci.edu',
-    long_description ='Numerical Analysis Algorithms from class.',
+    long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=['numpy'],
+    python_requires='3.8',
     url='https://github.com/mwstrand/numerical-analysis'
 )
